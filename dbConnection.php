c07502988a9cb288c747a9594185f3873f0444bc
Eng Mohanned Amer <mohannedamereng90@gmail.com>
2026-09-15 20:16:02 +0300

    Add dbConnection.php for MySQL database connection

---
+++ dbConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+if (!$mysqli) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+?>
